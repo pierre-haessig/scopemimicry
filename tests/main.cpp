c5528ea55b60b5cadcae69733e675cda19d1c976
Pierre Haessig <pierre.haessig@crans.org>
2025-03-30 07:40:59 +0200

    simplify + better diagnose scope test

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream> 
-#include <math.h> 
-#include <stdio.h> 
+/*Scope test */
+
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
 #include <stdint.h>
 
 #include "ScopeMimicry.h"
@@ -8,34 +10,51 @@
 static float32_t Ilow1;
 static float32_t Ilow2;
 
+const int n_channels = 2;
+
 bool mytrigger(void) {
+    return true;
     return (Ilow1 > 0.1);
 }
 
 int main(void) {
-    float32_t f0 = 50.0;
     float32_t t;
-    const float32_t Ts = 100e-6;
+    const float32_t Ts = 1e-3; // sampling time (s)
     uint8_t *buffer;
     uint16_t buffer_size = 0;
-    ScopeMimicry scope(201, 2);
+    int length_sim = 40; // simulation length -> 40 ms
+    uint16_t length = 10; // scope capture length
+    ScopeMimicry scope(length, n_channels);
     scope.connectChannel(Ilow1, "iLow1");
     scope.connectChannel(Ilow2, "iLow2");
     scope.set_trigger(&mytrigger);
-    scope.set_delay(0.5);
-    for (t=0; t <  4./f0; t+=Ts)  {
-        Ilow1 = (t < .5/f0) ? cos(2 * M_PI * f0 * t): 2*cos(2 * M_PI * f0 * t); 
-        Ilow2 = sin(2 * M_PI * f0 * t);
-        if (t > 2.0/f0) scope.start();
+    //scope.set_delay(0.5);
+    //scope.start();
+
+    for (int k=0; k<length_sim; k++)  {
+        t = k*Ts;
+        Ilow1 = t*1000;
+        Ilow2 = (k % 10)*0.1; // sawtooth 0 -> 0.9
+        if (k > 20) {
+            scope.start();
+        }
         uint16_t status = scope.acquire();
+        printf("k=%2d, t=%.3f: ", k, t);
+        printf("Ilow1=%.1f (status=%d)\n", Ilow1, status);
+
     }
 
     buffer = scope.get_buffer();
     buffer_size = scope.get_buffer_size()>>2;
     printf("%s,", scope.get_channel_name(0));
     printf("%s\n", scope.get_channel_name(1));
-    for (uint32_t k=0;k < (buffer_size);k=k+2)
+    printf("final_idx: %d\n", scope.get_final_idx());
+
+    for (uint32_t k=0;k < (buffer_size);k=k+n_channels)
     {
-        printf("%08x, %08x\n", *((uint32_t *)buffer+k), *((uint32_t *)buffer+k+1));
+        printf("%08x, %08x (%.2f A, %.2f A)\n",
+            *((uint32_t *)buffer+k), *((uint32_t *)buffer+k+1),
+            *((float32_t *)buffer+k), *((float32_t *)buffer+k+1)
+        );
     }
 }
